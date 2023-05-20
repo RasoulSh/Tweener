b9330a550ca7b71ec5e4496102483fc1eb167fb5
ShahRasoul <shahhosseini@unboundgamestudio.com>
2023-05-20 21:03:40 +0330

    :tada: Add UI Height and Width Tweener

---
+++ Runtime/Tweeners/UIHeightTweener.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using TweenerSystem.Enums;
+using UnityEngine;
+
+namespace TweenerSystem.Tweeners
+{
+    [RequireComponent(typeof(RectTransform))]
+    public class UIHeightTweener : SingleTweener
+    {
+        [field: SerializeField] public float From { get; set; }
+        [field: SerializeField] public float To { get; set; }
+        private RectTransform _thisRect;
+        private RectTransform ThisRect => _thisRect ??= GetComponent<RectTransform>();
+
+        protected override void Animate(float t)
+        {
+            var newHeight = Mathf.Lerp(From, To, t);
+            ThisRect.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, newHeight);
+        }
+    }
+}
---
+++ Runtime/Tweeners/UIWidthTweener.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using TweenerSystem.Enums;
+using UnityEngine;
+
+namespace TweenerSystem.Tweeners
+{
+    [RequireComponent(typeof(RectTransform))]
+    public class UIWidthTweener : SingleTweener
+    {
+        [field: SerializeField] public float From { get; set; }
+        [field: SerializeField] public float To { get; set; }
+        private RectTransform _thisRect;
+        private RectTransform ThisRect => _thisRect ??= GetComponent<RectTransform>();
+
+        protected override void Animate(float t)
+        {
+            var newWidth = Mathf.Lerp(From, To, t);
+            ThisRect.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, newWidth);
+        }
+    }
+}
